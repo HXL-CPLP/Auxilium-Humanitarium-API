82ea3fb39dea6da727037b9fe9d3f6c9bd9eacf9
Emerson Rocha <rocha@ieee.org>
2021-05-02 04:56:26 -0300

    CODV2API (#1): CODV2API-eng.openapi.yaml hand written started; we will use OpenAPI as main language instead of RAML

---
+++ scripts/prepare-hapi-pages.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+#===============================================================================
+#
+#          FILE:  prepare-hapi-pages.sh
+#
+#         USAGE:  ./prepare-hapi-pages.sh
+#
+#   DESCRIPTION:  DRAFT. May not be necessary (or too complex to require
+#                 dedicated effortover recomending use
+#                 https://editor.swagger.io/?url=http://example.org/openapi.yaml
+#
+#       OPTIONS:  ---
+#
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Emerson Rocha <rocha[at]ieee.org>
+#       COMPANY:  EticaAI
+#       LICENSE:  Public Domain dedication
+#                 SPDX-License-Identifier: Unlicense
+#       VERSION:  v0.1
+#       CREATED:  2021-05-02 06:10 UTC started
+#      REVISION:  ---
+#===============================================================================
+
+# TODO: https://docs.travis-ci.com/user/deployment/pages/
+
+#### openapi-generator (setup) _________________________________________________
+# @see https://openapi-generator.tech/docs/installation
+# npm install @openapitools/openapi-generator-cli -g
+# openapi-generator-cli version
+    # Download 5.1.0 ...
+    # Downloaded 5.1.0
+    # Did set selected version to 5.1.0
+    # 5.1.0
+
+# Online
+#    http://api.openapi-generator.tech/index.html
+
+#### Other comments (ignore) ___________________________________________________
+### opendocumenter broken, giving up for now
+# https://github.com/ouropencode/OpenDocumenter
+# npm install -g opendocumenter
+
+# https://openapi-generator.tech/docs/generators/html/
+
