f334ecc0cb29e78746b076a0969227286a43e5bb
Emerson Rocha <rocha@ieee.org>
2021-05-24 03:50:12 -0300

    mvp-tema-i18n (#9): gettext usa _data/L10n.hxl.csv

--- _plugins/jekyll_hapi_translationem.rb
+++ _plugins/jekyll_hapi_translationem.rb
@@ -13,20 +13,41 @@ module HapiApi
 
   # HapiApiGenerator is (TODO: document)
   class Translationem < Liquid::Tag
-    # def initialize(tag_name, text, tokens)
-    #   super
-    #   tokens = text.split
-
-    #   @text = tokens.shift
-    # end
     def initialize(tag_name, text, tokens)
       super
-      @text = text.strip
+
+      @tokens = text.strip.split
+      @text = @tokens.shift
+      @variant1 = @tokens.shift if @tokens
       # @datum = 123
     end
 
     def datum_temporarium(context)
       context['site']['data']['Temporarium']
+    end
+
+    def datum_l10n(l10n_codice, context, linguam = nil)
+      linguam = linguam.nil? ? context['page']['linguam'] : linguam
+      hxlattrs = context['site']['data']['referens']['linguam'][linguam]['hxlattrs']
+      # puts linguam
+      # puts hxlattrs
+      # puts context['site']['data']['L10nhxl']
+      context['site']['data']['L10nhxl'].each do |line|
+        # if line['#item+code'] == l10n_codice
+        next if line['#item+code'] != l10n_codice
+
+        hxlattrs.each do |hxlattr|
+          # puts hxlattr
+          next unless line["#item+l10n#{hxlattr}"]
+
+          # puts hxlattr
+          # puts line["#item+l10n#{hxlattr}"]
+
+          return line["#item+l10n#{hxlattr}"]
+        end
+      end
+
+      nil
     end
 
     def datum_temporarium_suffix(context)
@@ -52,14 +73,30 @@ module HapiApi
       @resultatum
     end
 
-    # TODO: esta meio feio isso. Melhorar
+    def de(text)
+      # print @tokens
+      # print @variant1
+      return de_markdown(text) if @variant1 && @variant1 == 'de_markdown'
+
+      text
+    end
+
+    # TODO: esta meio feio isso. Melhorar. Um problema é que cria
+    #       tags <p> mesmo em elementos inline.
     def de_markdown(text)
-      require "kramdown"
-      "#{Kramdown::Document.new(text).to_html}"
+      require 'kramdown'
+      Kramdown::Document.new(text).to_html.to_s
     end
 
     def render(context)
       temp = datum_temporarium(context)
+      l10nval = datum_l10n(@text, context)
+
+      # puts l10nval
+
+      return l10nval if l10nval
+
+      # puts context['site']['data']['L10nhxl']
 
       # puts context
       # # puts context.site
@@ -75,7 +112,8 @@ module HapiApi
       suffixes.each do |suffix|
         # puts ">#{@text}<"
         # puts "#{@text}#{suffix}"
-        return de_markdown(temp["#{@text}#{suffix}"]) if temp && temp["#{@text}#{suffix}"]
+        # return de_markdown(temp["#{@text}#{suffix}"]) if temp && temp["#{@text}#{suffix}"]
+        return de(temp["#{@text}#{suffix}"]) if temp && temp["#{@text}#{suffix}"]
       end
 
       # TODO: _[por] implementar _data/L10n.hxl.csv [por]_
