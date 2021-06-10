cac188b2ce1524167680cc6bb4ed462e738f8266
Emerson Rocha <rocha@ieee.org>
2021-06-10 01:37:37 -0300

    HXL-CPLP/forum#58, hapi-t9n-alpha (#13), mvp-l10n-schemas (#7): rascunho CSS e JS (MJS)

---
+++ _assets/README.md
@@ -0,0 +1,5 @@
+#_assets/README.md
+
+> TODO: talvez https://rdyar.github.io/2017/10/01/speed-up-jekyll-by-using-gulp-for-sass-and-other-assets/ (?)
+
+> TODO: talvez https://github.com/envygeeks/jekyll-assets (?)
---
+++ _assets/js/hapi-api.mjs
@@ -0,0 +1,8 @@
+/**
+ * @ARCHĪVUM       _assets/js/hapi-api.js
+ * @LICENTIAM      Dominium publicum
+ * @DĒSCRĪPTIŌNEM  _[por] Arquivo JavaScript carregado em todo site ao final
+ *                        de cada página.
+ *                 [por]_
+ */
+console.log('hapi-api.mjs')
