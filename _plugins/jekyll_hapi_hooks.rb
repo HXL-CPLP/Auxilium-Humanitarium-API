03565faa509d90326674430426690131d941a5c4
Emerson Rocha <rocha@ieee.org>
2021-06-14 04:41:07 -0300

    mvp-schemam-paginam (#14): schemam.formulam.structuram, rascunho

--- _plugins/jekyll_hapi_hooks.rb
+++ _plugins/jekyll_hapi_hooks.rb
@@ -57,6 +57,18 @@ Jekyll::Hooks.register :site, :post_write do |site| # rubocop:disable Metrics/Bl
 
   #### /data/ __________________________________________________________________
 
+  # if File.file?(File.join(site.dest, '/data/api.l10n.json')) && \
+  #    (!File.file?(File.join(site.source, '/_data/l10n/api.l10n.json')) || !FileUtils.compare_file(
+  #      File.join(site.dest, '/data/api.l10n.json'),
+  #      File.join(site.source, '/_data/l10n/api.l10n.json')
+  #    ))
+  #   puts 'jekyll_hapi_hooks: cp _site/data/api.l10n.json _data/api.l10n.json'
+  #   FileUtils.cp(
+  #     File.join(site.dest, '/data/api.l10n.json'),
+  #     File.join(site.source, '/_data/l10n/api.l10n.json')
+  #   )
+  # end
+
   if File.file?(File.join(site.dest, '/data/api.l10n.json')) && \
      (!File.file?(File.join(site.source, '/_data/l10n/api.l10n.json')) || !FileUtils.compare_file(
        File.join(site.dest, '/data/api.l10n.json'),
